afe97c4768726dc80aada98320aeac9f474c15c9
chetan094 <cjhampaty@gmail.com>
2026-09-25 18:34:01 +0530

    Production Deploy: BestPackerMovers v5.0 Next.js 14 Platform [25-09-2026 18:34:01.00]

--- next.config.js
+++ next.config.js
@@ -10,7 +10,7 @@ const nextConfig = {
     ],
   },
   experimental: {
-    serverComponentsExternalPackages: ['sqlite3', 'pg'],
+    serverComponentsExternalPackages: ['sql.js', 'pg'],
     outputFileTracingIncludes: {
       '/*': ['./database.sqlite'],
     },
